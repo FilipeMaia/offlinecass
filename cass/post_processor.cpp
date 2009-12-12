aef2ad6d6a250ee09744621c0c59a295d895634a
Filipe Maia <filipe@psexport.slac.stanford.edu>
2009-12-11 16:38:53 -0800

    Decreased the threshold to 30 variance

--- cass/post_processor.cpp
+++ cass/post_processor.cpp
@@ -331,7 +331,7 @@ bool cass::PostProcessor::isGoodImage(cass::CASSEvent &cassevent){
     good = false;
   }
   double stdDev = stdDevImage(cassevent,integral);
-  if(stdDev < sqrt(1000)){
+  if(stdDev < sqrt(30)){
     good = false;
   }
   return good;
