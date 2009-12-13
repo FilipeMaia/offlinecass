117ef88b8499ec4966920f515468f8eda6917862
Filipe Maia <filipe@psexport.slac.stanford.edu>
2009-12-12 16:23:23 -0800

    Make it flush the hdf5 files

--- cass/post_processor.cpp
+++ cass/post_processor.cpp
@@ -98,213 +98,218 @@ void postProcess_writeHDF5(cass::CASSEvent &cassevent) {
 	/*
 	 *	Simply return if there are no CCD frames!
 	 */
-		int nframes = cassevent.pnCCDEvent().detectors().size();
-		if (nframes == 0) {
-			printf("No pnCCD frames in this event:  skipping HDF5 write step...\n");
-			return;
-		}
-		
-		
-  	/*
-  	 *	Create filename based on date, time and LCLS fiducial for this image
-  	 */
-		char outfile[1024];
-		char buffer[1024];
-		Pds::Dgram *datagram = reinterpret_cast<Pds::Dgram*>(cassevent.datagrambuffer());
-		time_t eventTime = datagram->seq.clock().seconds();
-		//		time_t eventTimeNs = datagram->seq.clock().nanoseconds();
-		int32_t eventFiducial = datagram->seq.stamp().fiducials();
-		struct tm *timeinfo=localtime( &eventTime );
-		strftime(buffer,80,"LCLS_%Y_%b%d_%H%M%S",timeinfo);
-		sprintf(outfile,"%s_%i_pnCCD.h5",buffer,eventFiducial);
-		if(QFile::exists(outfile)){
-		  sprintf(outfile,"%s_%i_pnCCD-part2.h5",buffer,eventFiducial);
-		}
-		printf("Writing data to: %s\n",outfile);
-		
-		
-	/* 
-	 *	Create the HDF5 file
-	 */
-		hid_t 	hdf_fileID;
-		hid_t 	dataspace_id;
-		hid_t 	dataset_id;
-		hid_t 	datatype;
-		hsize_t dims[2];
-		herr_t 	hdf_error;
+  int nframes = cassevent.pnCCDEvent().detectors().size();
+  if (nframes == 0) {
+    printf("No pnCCD frames in this event:  skipping HDF5 write step...\n");
+    return;
+  }
+  
+  
+  /*
+   *	Create filename based on date, time and LCLS fiducial for this image
+   */
+  char outfile[1024];
+  char buffer[1024];
+  Pds::Dgram *datagram = reinterpret_cast<Pds::Dgram*>(cassevent.datagrambuffer());
+  time_t eventTime = datagram->seq.clock().seconds();
+  //		time_t eventTimeNs = datagram->seq.clock().nanoseconds();
+  int32_t eventFiducial = datagram->seq.stamp().fiducials();
+  struct tm *timeinfo=localtime( &eventTime );
+  strftime(buffer,80,"LCLS_%Y_%b%d_%H%M%S",timeinfo);
+  sprintf(outfile,"%s_%i_pnCCD.h5",buffer,eventFiducial);
+  if(QFile::exists(outfile)){
+    sprintf(outfile,"%s_%i_pnCCD-part2.h5",buffer,eventFiducial);
+  }
+  printf("Writing data to: %s\n",outfile);
+  
+  
+  /* 
+   *	Create the HDF5 file
+   */
+  hid_t 	hdf_fileID;
+  hid_t 	dataspace_id;
+  hid_t 	dataset_id;
+  hid_t 	datatype;
+  hsize_t dims[2];
+  herr_t 	hdf_error;
+  hid_t   gid;
+  /* check if file exists */
+  
+  hdf_fileID = H5Fcreate(outfile,  H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+  
+  
+  /*
+   *	Write pnCCD frames to '/data' part of HDF5 file
+   */
+  gid = H5Gcreate1(hdf_fileID,"data",0);
+  
+  // Save each pnCCD frame in the XTC data set
+  int skipped = 0;
+  for(int i=0; i<nframes; i++) {
+    int rows = cassevent.pnCCDEvent().detectors()[i].rows();
+    int columns = cassevent.pnCCDEvent().detectors()[i].columns();
+    if(!rows || !columns){
+      skipped++;
+      printf("pnCCD frame with ilogical size %dx%d!\n",columns,rows);
+      continue;
+    }
+    int16_t *data = &cassevent.pnCCDEvent().detectors()[i].correctedFrame()[0];
+    
+    char fieldname[100]; 
+    sprintf(fieldname,"/data/data%i",i-skipped);
+    
+    dims[0] = rows;
+    dims[1] = columns;
+    dataspace_id = H5Screate_simple( 2, dims, NULL);
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+    if( H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data)< 0){
+      printf("Error when writing data %i...\n",i);
+      return;
+    }
+    H5Dclose(dataset_id);
+    H5Sclose(dataspace_id);
+  }
+  
+  // Save information on number of pnCCD frames saved
+  dims[0] = 1;
+  dataspace_id = H5Screate_simple( 1, dims, NULL );
+  int adjusted_nframes = nframes-skipped;
+  dataset_id = H5Dcreate1(hdf_fileID, "/data/nframes", H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,&adjusted_nframes );
+  H5Dclose(dataset_id);
+  H5Sclose(dataspace_id);
+  
+  // Create symbolic link from /data/data0 to /data/data 
+  // (to maintain our convention of /data/data always containing data)
+  hdf_error = H5Lcreate_soft( "/data/data0", hdf_fileID, "/data/data",0,0);
+  
+  
+  /*
+   *	Write pnCCD configurations
+   */
+  H5Gclose(gid);
+  gid = H5Gcreate1(hdf_fileID,"pnCCD",0);
+  dims[0] = 1;
+  dataspace_id = H5Screate_simple( 1, dims, NULL );
+  
+  dataset_id = H5Dcreate1(hdf_fileID, "/pnCCD/n_CCDs", H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT,&nframes );
+  H5Dclose(dataset_id);
+  H5Sclose(dataspace_id);
+  
+  skipped = 0;
+  for(int i=0; i<nframes; i++) {
+    char fieldname[100]; 
+    int ccd_index = i-skipped;
+    int rows = cassevent.pnCCDEvent().detectors()[i].rows();
+    int columns = cassevent.pnCCDEvent().detectors()[i].columns();
+    
+    if(!rows || !columns){
+      printf("pnCCD frame with ilogical size %dx%d!\n",columns,rows);
+      skipped++;
+      continue;
+    }
+    sprintf(fieldname,"/pnCCD/pnCCD%i",ccd_index);
+    hid_t gid = H5Gcreate1(hdf_fileID,fieldname,0);
+    dataspace_id = H5Screate_simple( 1, dims, NULL );
+    
+    sprintf(fieldname,"/pnCCD/pnCCD%i/rows",ccd_index);
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].rows() );
+    H5Dclose(dataset_id);
+    
+    sprintf(fieldname,"/pnCCD/pnCCD%i/columns",ccd_index);
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].columns() );
+    H5Dclose(dataset_id);
+    
+    sprintf(fieldname,"/pnCCD/pnCCD%i/originalrows",ccd_index);
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].originalrows() );
+    H5Dclose(dataset_id);
+    
+    sprintf(fieldname,"/pnCCD/pnCCD%i/originalcolumns",ccd_index);
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].originalcolumns() );
+    H5Dclose(dataset_id);
+    
+    sprintf(fieldname,"/pnCCD/pnCCD%i/row_binning",ccd_index);
+    int16_t rbin = cassevent.pnCCDEvent().detectors()[0].originalrows()/cassevent.pnCCDEvent().detectors()[i].rows();
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &rbin);
+    H5Dclose(dataset_id);
+    
+    sprintf(fieldname,"/pnCCD/pnCCD%i/column_binning",ccd_index);
+    int16_t cbin = cassevent.pnCCDEvent().detectors()[0].originalcolumns()/cassevent.pnCCDEvent().detectors()[i].columns();
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cbin);
+    H5Dclose(dataset_id);
+    
+    sprintf(fieldname,"/pnCCD/pnCCD%i/integral",ccd_index);
+    dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_LONG, dataspace_id, H5P_DEFAULT);
+    H5Dwrite(dataset_id, H5T_NATIVE_LONG, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].integral() );
+    H5Dclose(dataset_id);
+    
+    
+    H5Sclose(dataspace_id);
+    H5Gclose(gid);
+    
+  }
+  
+  /*
+   *	Write LCLS event information
+   */
+  gid = H5Gcreate1(hdf_fileID,"LCLS",0);
+  dims[0] = 1;
+  dataspace_id = H5Screate_simple( 1, dims, NULL );
+  
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/machineTime", H5T_NATIVE_UINT32, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_UINT32, H5S_ALL, H5S_ALL, H5P_DEFAULT, &eventTime );
+  H5Dclose(dataset_id);
+  
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/fiducial", H5T_NATIVE_INT32, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_INT32, H5S_ALL, H5S_ALL, H5P_DEFAULT, &eventFiducial );
+  H5Dclose(dataset_id);
+  
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/energy", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().energy() );
+  H5Dclose(dataset_id);
+  
 
-		/* check if file exists */
-
-		hdf_fileID = H5Fcreate(outfile,  H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-
-
-	/*
-	 *	Write pnCCD frames to '/data' part of HDF5 file
-	 */
-		H5Gcreate1(hdf_fileID,"data",0);
-
-		// Save each pnCCD frame in the XTC data set
-		int skipped = 0;
-	 	for(int i=0; i<nframes; i++) {
-			int rows = cassevent.pnCCDEvent().detectors()[i].rows();
-			int columns = cassevent.pnCCDEvent().detectors()[i].columns();
-			if(!rows || !columns){
-			  skipped++;
-			  printf("pnCCD frame with ilogical size %dx%d!\n",columns,rows);
-			  continue;
-			}
-			int16_t *data = &cassevent.pnCCDEvent().detectors()[i].correctedFrame()[0];
-
-	 		char fieldname[100]; 
-	 		sprintf(fieldname,"/data/data%i",i-skipped);
-
-		    dims[0] = rows;
-		    dims[1] = columns;
-			dataspace_id = H5Screate_simple( 2, dims, NULL);
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-			if( H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data)< 0){
-				printf("Error when writing data %i...\n",i);
-				return;
-			}
-			H5Dclose(dataset_id);
-    		H5Sclose(dataspace_id);
-	 	}
-	 	
-		// Save information on number of pnCCD frames saved
-	    dims[0] = 1;
-	    dataspace_id = H5Screate_simple( 1, dims, NULL );
-	    int adjusted_nframes = nframes-skipped;
-	    dataset_id = H5Dcreate1(hdf_fileID, "/data/nframes", H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,&adjusted_nframes );
-	    H5Dclose(dataset_id);
-		H5Sclose(dataspace_id);
-
-		// Create symbolic link from /data/data0 to /data/data 
-		// (to maintain our convention of /data/data always containing data)
-		hdf_error = H5Lcreate_soft( "/data/data0", hdf_fileID, "/data/data",0,0);
-
-
-	/*
-	 *	Write pnCCD configurations
-	 */
-		H5Gcreate1(hdf_fileID,"pnCCD",0);
-	    dims[0] = 1;
-	    dataspace_id = H5Screate_simple( 1, dims, NULL );
-
-	    dataset_id = H5Dcreate1(hdf_fileID, "/pnCCD/n_CCDs", H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT,&nframes );
-	    H5Dclose(dataset_id);
-
-	    skipped = 0;
-	 	for(int i=0; i<nframes; i++) {
-	 		char fieldname[100]; 
-			int ccd_index = i-skipped;
-			int rows = cassevent.pnCCDEvent().detectors()[i].rows();
-			int columns = cassevent.pnCCDEvent().detectors()[i].columns();
-
-			if(!rows || !columns){
-			  printf("pnCCD frame with ilogical size %dx%d!\n",columns,rows);
-			  skipped++;
-			  continue;
-			}
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i",ccd_index);
-			H5Gcreate1(hdf_fileID,fieldname,0);
-			dataspace_id = H5Screate_simple( 1, dims, NULL );
-			
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i/rows",ccd_index);
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-			H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].rows() );
-			H5Dclose(dataset_id);
-
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i/columns",ccd_index);
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-			H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].columns() );
-			H5Dclose(dataset_id);
-
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i/originalrows",ccd_index);
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-			H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].originalrows() );
-			H5Dclose(dataset_id);
-
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i/originalcolumns",ccd_index);
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-			H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].originalcolumns() );
-			H5Dclose(dataset_id);
-
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i/row_binning",ccd_index);
-	 		int16_t rbin = cassevent.pnCCDEvent().detectors()[0].originalrows()/cassevent.pnCCDEvent().detectors()[i].rows();
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-			H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &rbin);
-			H5Dclose(dataset_id);
-
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i/column_binning",ccd_index);
-	 		int16_t cbin = cassevent.pnCCDEvent().detectors()[0].originalcolumns()/cassevent.pnCCDEvent().detectors()[i].columns();
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_SHORT, dataspace_id, H5P_DEFAULT);
-			H5Dwrite(dataset_id, H5T_NATIVE_SHORT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cbin);
-			H5Dclose(dataset_id);
-
-	 		sprintf(fieldname,"/pnCCD/pnCCD%i/integral",ccd_index);
-			dataset_id = H5Dcreate1(hdf_fileID, fieldname, H5T_NATIVE_LONG, dataspace_id, H5P_DEFAULT);
-			H5Dwrite(dataset_id, H5T_NATIVE_LONG, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.pnCCDEvent().detectors()[i].integral() );
-			H5Dclose(dataset_id);
-
-    		H5Sclose(dataspace_id);
-	 	}
-
-	/*
-	 *	Write LCLS event information
-	 */
-		H5Gcreate1(hdf_fileID,"LCLS",0);
-	    dims[0] = 1;
-	    dataspace_id = H5Screate_simple( 1, dims, NULL );
-
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/machineTime", H5T_NATIVE_UINT32, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_UINT32, H5S_ALL, H5S_ALL, H5P_DEFAULT, &eventTime );
-	    H5Dclose(dataset_id);
-
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/fiducial", H5T_NATIVE_INT32, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_INT32, H5S_ALL, H5S_ALL, H5P_DEFAULT, &eventFiducial );
-	    H5Dclose(dataset_id);
-
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/energy", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().energy() );
-	    H5Dclose(dataset_id);
-
-
-		// Gas detector values
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_11_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_11_ENRC() );
-	    H5Dclose(dataset_id);
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_12_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_12_ENRC() );
-	    H5Dclose(dataset_id);
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_21_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_21_ENRC() );
-	    H5Dclose(dataset_id);
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_22_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_22_ENRC() );
-	    H5Dclose(dataset_id);
-
-
-	    dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/ebeamCharge", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().EbeamCharge() );
-	    H5Dclose(dataset_id);
-
-		H5Sclose(dataspace_id);
-
-		// Strings are a little tricky --> this could be improved!
-		char* timestr;
-		timestr = ctime(&eventTime);
-		dataspace_id = H5Screate(H5S_SCALAR);
-	  	datatype = H5Tcopy(H5T_C_S1);  
-	  	H5Tset_size(datatype,strlen(timestr));
-	    dataset_id = H5Dcreate1(hdf_fileID, "LCLS/eventTime", datatype, dataspace_id, H5P_DEFAULT);
-	    H5Dwrite(dataset_id, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, timestr );
-	    H5Dclose(dataset_id);
-	    H5Sclose(dataspace_id);
-	    H5Fclose(hdf_fileID);
-
-		
+  // Gas detector values
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_11_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_11_ENRC() );
+  H5Dclose(dataset_id);
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_12_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_12_ENRC() );
+  H5Dclose(dataset_id);
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_21_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_21_ENRC() );
+  H5Dclose(dataset_id);
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/f_22_ENRC", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().f_22_ENRC() );
+  H5Dclose(dataset_id);
+  
+  
+  dataset_id = H5Dcreate1(hdf_fileID, "/LCLS/ebeamCharge", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &cassevent.MachineDataEvent().EbeamCharge() );
+  H5Dclose(dataset_id);
+  
+  H5Sclose(dataspace_id);
+  
+  // Strings are a little tricky --> this could be improved!
+  char* timestr;
+  timestr = ctime(&eventTime);
+  dataspace_id = H5Screate(H5S_SCALAR);
+  datatype = H5Tcopy(H5T_C_S1);  
+  H5Tset_size(datatype,strlen(timestr));
+  dataset_id = H5Dcreate1(hdf_fileID, "LCLS/eventTime", datatype, dataspace_id, H5P_DEFAULT);
+  H5Dwrite(dataset_id, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, timestr );
+  H5Dclose(dataset_id);
+  H5Sclose(dataspace_id);
+  H5Gclose(gid);
+  H5Fflush(hdf_fileID,H5F_SCOPE_LOCAL);
+  H5Fclose(hdf_fileID); 
 }
 
 
